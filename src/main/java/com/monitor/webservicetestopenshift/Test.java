7a6f635692de00e8ae6121b7c9ace0ff25bfe174
supriyabaidya <33324876+supriyabaidya@users.noreply.github.com>
2018-04-24 22:46:16 +0530

    android WebService Test openshift 3.0.night

--- src/main/java/com/monitor/webservicetestopenshift/Test.java
+++ src/main/java/com/monitor/webservicetestopenshift/Test.java
@@ -8,14 +8,12 @@ package com.monitor.webservicetestopenshift;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
-import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.Style;
 
 /**
  *
  * @author TomHardy
  */
-//@SOAPBinding(style = Style.RPC)
+
 @WebService(serviceName = "Test")
 public class Test {
 
