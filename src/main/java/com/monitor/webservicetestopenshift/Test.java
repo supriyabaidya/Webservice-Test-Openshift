153ef5c73bd28c40b362c29a2242a51741db424c
supriyabaidya <33324876+supriyabaidya@users.noreply.github.com>
2018-04-24 22:44:05 +0530

    android WebService Test openshift 3.0.night

--- src/main/java/com/monitor/webservicetestopenshift/Test.java
+++ src/main/java/com/monitor/webservicetestopenshift/Test.java
@@ -8,11 +8,15 @@ package com.monitor.webservicetestopenshift;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
 
 /**
  *
  * @author TomHardy
  */
+//@SOAPBinding(style = Style.RPC)
+
 @WebService(serviceName = "Test")
 public class Test {
 
@@ -25,5 +29,20 @@ public class Test {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+
+    @WebMethod(operationName = "twoDimesionArray")
+    public String twoDimesionArray(@WebParam(name = "array") String[][] array) {
+        int noOfRow = array.length;
+        int noOfColomn;
+
+        for (int i = 0; i < noOfRow; i++) {
+
+            noOfColomn = array[i].length;
+            for (int j = 0; j < noOfColomn; j++) {
+                System.out.println(i + " , " + j + " => " + array[i][j]);
+            }
+        }
+        return "success";
     }
 }
