d00200f5b6c56b11c5e299b572bcde0885e093e1
supriyabaidya <33324876+supriyabaidya@users.noreply.github.com>
2018-04-27 17:31:11 +0530

    WebService Test openshift 3.4

--- src/main/java/com/monitor/webservicetestopenshift/Test.java
+++ src/main/java/com/monitor/webservicetestopenshift/Test.java
@@ -5,6 +5,13 @@
  */
 package com.monitor.webservicetestopenshift;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -13,7 +20,6 @@ import javax.jws.WebParam;
  *
  * @author TomHardy
  */
-
 @WebService(serviceName = "Test")
 public class Test {
 
@@ -29,7 +35,7 @@ public class Test {
     }
 
     @WebMethod(operationName = "twoDimesionArray")
-    public String twoDimesionArray(@WebParam(name = "array") String[][] array) {
+    public String twoDimesionArray(@WebParam(name = "username") String username, @WebParam(name = "array") String[][] array) {
         int noOfRow = array.length;
         int noOfColomn;
 
@@ -40,6 +46,53 @@ public class Test {
                 System.out.println(i + " , " + j + " => " + array[i][j]);
             }
         }
+
+        return putCSV(username, array);
+    }
+
+    private String putCSV(String username, String[][] data) {
+
+        System.out.println("com.monitor.webservicetestopenshift.Test.putCSV()" + System.getProperty("user.dir"));
+        String generatedFilesPath = System.getProperty("user.dir") + "\\generatedFiles";
+        File generatedFilesDirectory = new File(generatedFilesPath);
+
+        if (!generatedFilesDirectory.exists()) {
+            generatedFilesDirectory.mkdirs();
+        }
+
+        File coverageR = new File(generatedFilesPath + "\\" + username + "_coverageR.csv");
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+
+        try {
+            fileWriter = new FileWriter(coverageR);
+            bufferedWriter = new BufferedWriter(fileWriter);
+
+            int row = data.length;
+            int column;
+            for (int i = 0; i < row; i++) {
+                column = data[i].length;
+                for (int j = 0; j < column; j++) {
+                    bufferedWriter.write(data[i][j]);
+                    if (j < column - 1) {
+                        bufferedWriter.write(",");
+                    } else {
+                        bufferedWriter.write("\n");
+                    }
+                }
+            }
+
+            bufferedWriter.close();
+            fileWriter.close();
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+            return ex.toString();
+        } catch (IOException ex) {
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+            return ex.toString();
+        }
+
         return "success";
     }
 }
