bdad61d7522fa4720d9f1df782ac6bae5d78af70
supriyabaidya <33324876+supriyabaidya@users.noreply.github.com>
2018-05-01 23:15:36 +0530

    webservice test 6.0

--- src/main/java/com/monitor/webservicetestopenshift/Test.java
+++ src/main/java/com/monitor/webservicetestopenshift/Test.java
@@ -75,8 +75,8 @@ public class Test {
             Class.forName("com.mysql.jdbc.Driver");
             if (connection == null) {
 //                connection = DriverManager.getConnection("jdbc:mysql://johnny.heliohost.org/supriyo_sensor_cloud?useSSL=false", "supriyo_63", "sb@9051568624");
-//                connection = DriverManager.getConnection("jdbc:mysql://localhost/sensor_cloud?useSSL=false", "root", "");
-                connection = DriverManager.getConnection("jdbc:mysql://jws-app-mysql-1-qgftg:3306/sensor_cloud?useSSL=false", "userTY1", "VQvd3Ea0");
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sensor_cloud?useSSL=false", "root", "");
+//                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sensor_cloud?useSSL=false", "userTY1", "VQvd3Ea0");
             }
             statement = connection.createStatement();
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -12,7 +12,7 @@
         <title>Monitor Test</title>
     </head>
     <body>
-        <h1>Hello World ! </br>commit : 5.9</h1>
+        <h1>Hello World ! </br>commit : 6.0</h1>
 
 
 
